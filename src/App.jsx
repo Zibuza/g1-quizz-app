3350ef90be8bf0cecb1ec174d1bb96e1ea5f9d39
Luka Koridze <lukakoridze13@gmail.com>
2025-02-22 16:51:47 +0400

    init of tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
 const App = () => {
+  const [colorTheme, setColorTheme] = useState("light");
+  useEffect(() => {
+    if (colorTheme === "light") {
+      document.querySelector("html").classList.remove("dark");
+    } else {
+      document.querySelector("html").classList.add("dark");
+    }
+  }, [colorTheme]);
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <main className="w-full light h-[100dvh] bg-light-gray dark:bg-dark-navy">
+      <button
+        onClick={() => {
+          colorTheme === "light"
+            ? setColorTheme("dark")
+            : setColorTheme("light");
+        }}
+      >
+        {colorTheme}
+      </button>
+    </main>
+  );
+};
 
-export default App
+export default App;
